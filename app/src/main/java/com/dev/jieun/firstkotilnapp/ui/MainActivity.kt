fad99256f280b7f61f05fcf0556cd46f71c679d8
jieundev <jlel0002@gmail.com>
2017-10-01 19:00:34 +0900

    -새로운 하위 디렉토리 생성 및 위치 수정

--- app/src/main/java/com/dev/jieun/firstkotilnapp/ui/MainActivity.kt
+++ app/src/main/java/com/dev/jieun/firstkotilnapp/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.dev.jieun.firstkotilnapp
+package com.dev.jieun.firstkotilnapp.ui
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.dev.jieun.firstkotilnapp.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
---
+++ app/src/main/java/com/dev/jieun/firstkotilnapp/ui/base/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.jieun.firstkotilnapp.ui.base
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Administrator on 2017-10-01.
+ */
+class BaseActivity : AppCompatActivity () {
+
+}
