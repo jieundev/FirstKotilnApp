f25ff3a2d5a8c63a671c5d45993f053f27ebdb1e
jieundev <jlel0002@gmail.com>
2017-10-10 14:14:53 +0900

    Update MainActivity.kt

--- app/src/main/java/com/dev/jieun/firstkotilnapp/ui/MainActivity.kt
+++ app/src/main/java/com/dev/jieun/firstkotilnapp/ui/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.dev.jieun.firstkotilnapp.ui
 
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import com.dev.jieun.firstkotilnapp.R
 import com.dev.jieun.firstkotilnapp.ui.base.BaseActivity
 import com.dev.jieun.firstkotilnapp.util.replaceFragmentToActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+    private val toolbar: Toolbar by lazy {
+        findViewById<Toolbar>(R.id.toolbar) as Toolbar
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setArrowToolbar(getString(R.string.app_name))
-
+        toolbar.title = getString(R.string.app_name)
         replaceFragmentToActivity(MainFragment.INSTANCE, R.id.layFrameRoot, MainFragment.TAG)
     }
 }
