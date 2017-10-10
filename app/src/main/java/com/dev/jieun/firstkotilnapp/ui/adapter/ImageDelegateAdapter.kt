d0a505dfa877839d0324042739d5a6a56d02036c
jieundev <jlel0002@gmail.com>
2017-10-10 12:30:50 +0900

    Update ImageDelegateAdapter.kt
    constructor 수정

--- app/src/main/java/com/dev/jieun/firstkotilnapp/ui/adapter/ImageDelegateAdapter.kt
+++ app/src/main/java/com/dev/jieun/firstkotilnapp/ui/adapter/ImageDelegateAdapter.kt
@@ -3,8 +3,10 @@ package com.dev.jieun.firstkotilnapp.ui.adapter
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.dev.jieun.firstkotilnapp.R
 import com.dev.jieun.firstkotilnapp.data.model.Photo
 import kotlinx.android.synthetic.main.item_photo_view.view.*
@@ -12,7 +14,7 @@ import kotlinx.android.synthetic.main.item_photo_view.view.*
 /**
  * Created by Administrator on 2017-10-09.
  */
-class ImageDelegateAdapter(val context: Context) : ViewDelegateAdapter {
+class ImageDelegateAdapter(val context: Context, val itemClick: (Photo)->Unit) : ViewDelegateAdapter {
 
     override fun onCreateViewHolder(parent: ViewGroup, context: Context): RecyclerView.ViewHolder {
         // println("image delegate create")
@@ -29,7 +31,10 @@ class ImageDelegateAdapter(val context: Context) : ViewDelegateAdapter {
         fun onBind(photo: Photo){
             // println("call glide1 url : " +photo.getImageUrl())
             itemView.run {
-                Glide.with(context).load(photo.getImageUrl()).into(img_photo)
+                setOnClickListener {
+                    itemClick(photo)
+                }
+                Glide.with(context).load(photo.getImageUrl()).apply(RequestOptions().fitCenter()).into(img_photo)
                 // println("call glide2 url : " +photo.getImageUrl())
             }
         }
