a44095cd45ce7ac8c6b83d06fe7511dc894002e5
jieundev <jlel0002@gmail.com>
2017-10-10 12:30:11 +0900

    Update Photo.kt
    Photo extened Parcelable

--- app/src/main/java/com/dev/jieun/firstkotilnapp/data/model/Photo.kt
+++ app/src/main/java/com/dev/jieun/firstkotilnapp/data/model/Photo.kt
@@ -1,5 +1,7 @@
 package com.dev.jieun.firstkotilnapp.data.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.dev.jieun.firstkotilnapp.ui.adapter.LoadMoreAdapter
 import com.dev.jieun.firstkotilnapp.ui.adapter.ViewType
 import com.dev.jieun.firstkotilnapp.util.AdapterConstants
@@ -13,10 +15,47 @@ data class Photo (
         val title: String,
         val ispublic: Int,
         val isfriend: Int,
-        val isfamily: Int) : ViewType {
+        val isfamily: Int) : ViewType, Parcelable {
+
+    constructor(parcel: Parcel) : this (
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readInt(),
+            parcel.readString(),
+            parcel.readInt(),
+            parcel.readInt(),
+            parcel.readInt()
+    )
 
     override fun getViewType(): Int = AdapterConstants.ITEM
     fun getImageUrl(): String
             = "https://farm$farm.staticflickr.com/$server/${id}_${secret}_c.jpg"
 
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeString(id)
+        dest?.writeString(owner)
+        dest?.writeString(secret)
+        dest?.writeString(server)
+        dest?.writeInt(farm)
+        dest?.writeString(title)
+        dest?.writeInt(ispublic)
+        dest?.writeInt(isfriend)
+        dest?.writeInt(isfamily)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Photo> {
+        override fun createFromParcel(parcel: Parcel): Photo {
+            return Photo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Photo?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
